f4fe89729d24dfc1352f2e797d57091237d40e69
Devanshu Markam <devanshuasm750@gmail.com>
2025-10-14 12:48:51 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom'; 
 
+//find div in index.html with id 'root' and take full control of it
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Wrap the entire App component with BrowserRouter */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
+
+//finds a spot in html , activates the routing system , and then renders the app component
