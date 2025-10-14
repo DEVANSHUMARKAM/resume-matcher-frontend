6edc25bba428a5862725e8b3ffcb7ae92988a5ff
Devanshu Markam <devanshuasm750@gmail.com>
2025-10-14 12:47:51 +0530

    Landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,9 +1,5 @@
-// src/components/LandingPage.js
-
-import React from 'react';
-import { Typography, Box, Button, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import { Box, Button, List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import { TypeAnimation } from 'react-type-animation';
 import { useNavigate } from 'react-router-dom';
 
 function LandingPage() {
@@ -24,26 +20,6 @@ function LandingPage() {
         gap: 4
       }}
     >
-      {/* HERE IS THE FIX:
-        We wrap the animation in a Box with a minimum height.
-        This Box reserves the vertical space, preventing the content below from shifting.
-      */}
-      {/* <Box sx={{ minHeight: '120px', display: 'flex', alignItems: 'center' }}>
-        <TypeAnimation
-          sequence={[
-            'THE INTELLIGENT RESUME MATCHER',
-            1500,
-            '',
-            500,
-          ]}
-          wrapper="h1"
-          speed={50}
-          style={{ fontSize: '2.5rem', fontWeight: 'bold' }} // We can remove minHeight from here
-          repeat={Infinity}
-        />
-      </Box> */}
-
-      {/* The rest of the component is the same */}
       <List sx={{ maxWidth: 900 }}>
         <ListItem>
           <ListItemIcon>
@@ -86,3 +62,6 @@ function LandingPage() {
 }
 
 export default LandingPage;
+
+//This is the first page a user sees. Its job is to introduce the 
+// project and provide a single, clear "call to action" with the "Get Started" button.
